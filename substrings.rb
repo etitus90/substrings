4060a39593790e5c106e183e40e50b029136b847
etitus90 <etitus@protonmail.com>
2022-08-16 12:47:42 -0700

    Write double iterator method

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,19 @@
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
-def substrings(word, dictionary)
-    
+def substrings(string, dictionary)
+    array = string.split(' ')
+    hash = {}
+    array.each do |word|
+        dictionary.each do |dict_word|
+            word.include?(dict_word)
+                if (hash[dict_word])
+                    hash[dict_word] += 1
+                else
+                    hash[dict_word] = 1
+                end
+        end
+    end
+    puts hash
 end
+
+substrings("below", dictionary)
